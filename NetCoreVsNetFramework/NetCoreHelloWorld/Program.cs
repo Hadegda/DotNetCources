51e03b7782ab9f955e4d0771a8b1b5273789ff90
nadezhda <katsman96@yandex.ru>
2023-10-10 21:20:58 +0400

    .Net Fundamentals Task 1

---
+++ NetCoreVsNetFramework/NetCoreHelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCoreHelloWorld
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var username = args.Length > 0 ? args[0] : "NoName";
+			Console.WriteLine($"Hello, {username}");
+		}
+	}
+}
---
+++ NetCoreVsNetFramework/WpfAppHelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace WpfAppHelloWorld
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		public string Username
+		{
+			get => _username;
+			set
+			{ 
+				_username = value;
+				NotifyPropertyChanged(nameof(Username));
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private string _username = "NoName";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			DataContext = this;
+		}
+
+		private void NotifyPropertyChanged(string propertyName) =>
+			PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
