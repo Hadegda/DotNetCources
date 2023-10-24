bd8fdda03e401de20da9c2c8f7cb448a18a19886
nadezhda <katsman96@yandex.ru>
2023-10-24 19:35:42 +0400

    Override Equals and GwtHashCode methods for Product class

--- Debugging Fundamentals/Task1/Product.cs
+++ Debugging Fundamentals/Task1/Product.cs
@@ -1,15 +1,35 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     public class Product
-    {
-        public Product(string name, double price)
+	{
+		public string Name { get; set; }
+		public double Price { get; set; }
+
+		public Product(string name, double price)
         {
             Name = name;
             Price = price;
         }
 
-        public string Name { get; set; }
 
-        public double Price { get; set; }
-    }
+		public override bool Equals(Object obj)
+		{
+			if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+			{
+				return false;
+			}
+			else
+			{
+				Product p = (Product)obj;
+				return (Name == p.Name) && (Price == p.Price);
+			}
+		}
+
+		public override int GetHashCode()
+		{
+			return (Name.GetHashCode() << 2) ^ Price.GetHashCode();
+		}
+	}
 }
