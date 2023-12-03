666bf088ab0b32c096fc51606a783d1584f1dea0
nadezhda <katsman96@yandex.ru>
2023-12-04 00:36:14 +0400

    Implement  task 2 for module 6

--- M6_DataStructures/DataStructures/Tasks/HybridFlowProcessor.cs
+++ M6_DataStructures/DataStructures/Tasks/HybridFlowProcessor.cs
@@ -5,24 +5,34 @@ namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
-        public T Dequeue()
+        private readonly DoublyLinkedList<T> _values = new DoublyLinkedList<T>();
+
+		public T Dequeue()
         {
-            throw new NotImplementedException();
-        }
+            if (_values.Length == 0)
+            {
+				throw new InvalidOperationException();
+			}
+			return _values.RemoveAt(0);
+		}
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
-        }
+			_values.Add(item);
+		}
 
         public T Pop()
-        {
-            throw new NotImplementedException();
+		{
+			if (_values.Length == 0)
+			{
+				throw new InvalidOperationException();
+			}
+			return _values.RemoveAt(_values.Length - 1);
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _values.Add(item);
         }
     }
 }
