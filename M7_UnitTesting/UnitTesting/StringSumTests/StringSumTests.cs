feb35bf1f6b28d685efe57fcaf8758a0079a28bb
nadezhda <katsman96@yandex.ru>
2023-12-06 22:50:50 +0400

    M7 Create solution and projects for task 1

---
+++ M7_UnitTesting/UnitTesting/StringSumTests/StringSumTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting.Tests
+{
+	[TestClass()]
+	public class StringSumTests
+	{
+		[TestMethod()]
+		public void SumTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
---
+++ M7_UnitTesting/UnitTesting/UnitTesting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTesting
+{
+	public class StringSum
+	{
+		public static string Sum(string num1, string num2)
+		{
+			return "";
+		}
+	}
+}
