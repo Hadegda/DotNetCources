6b3f1f828fde1c7d0b3273acaa8036b7e875d516
nadezhda <katsman96@yandex.ru>
2023-12-12 21:31:21 +0400

    Improve code to pass test for negative and fractional numbers

--- M7_UnitTesting/UnitTesting/UnitTesting/StringSum.cs
+++ M7_UnitTesting/UnitTesting/UnitTesting/StringSum.cs
@@ -6,12 +6,18 @@ namespace UnitTesting.StringSum
 	{
 		public static string Sum(string num1, string num2)
 		{
-			if (!int.TryParse(num1, out int number1) || !int.TryParse(num2, out int number2))
+			if (!double.TryParse(num1, out double number1) || !double.TryParse(num2, out double number2))
 			{
 				throw new ArgumentOutOfRangeException();
 			}
 
-			return ((long)number1 + number2).ToString();
+			number1 = IsNatural(number1) ? number1 : 0;
+			number2 = IsNatural(number2) ? number2 : 0;
+
+			return (number1 + number2).ToString();
 		}
+
+		private static bool IsNatural(double number) => 
+			number >= 0 && number == Math.Round(number);
 	}
 }
