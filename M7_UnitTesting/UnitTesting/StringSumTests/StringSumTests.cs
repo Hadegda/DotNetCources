fbdc6064407db2b1cd70d275525f364bd4ef02cf
nadezhda <katsman96@yandex.ru>
2023-12-12 21:02:27 +0400

    Add unit test to test sum for small values

--- M7_UnitTesting/UnitTesting/StringSumTests/StringSumTests.cs
+++ M7_UnitTesting/UnitTesting/StringSumTests/StringSumTests.cs
@@ -14,11 +14,20 @@ namespace UnitTesting.StringSum.Tests
 		[DataRow("4fg", "5")]
 		[DataRow("5", "-4gf")]
 		[DataRow("+4fg", "5")]
-		[DataRow("5", null)]
-		[DataRow(null, "5")]
 		public void SumTest_InvalidArguments_ThrowsException(string num1, string num2)
 		{
 			StringSum.Sum(num1, num2);
+		}
+
+		[TestMethod]
+		[DataRow("3", "8", "11")]
+		[DataRow("0", "1", "1")]
+		[DataRow("1", "0", "1")]
+		public void SumTest_ValidArguments_CountCorrectly(string num1, string num2, string expectedValue)
+		{
+			var actualValue = StringSum.Sum(num1, num2);
+
+			Assert.AreEqual(expectedValue, actualValue);
 		}
 	}
 }
