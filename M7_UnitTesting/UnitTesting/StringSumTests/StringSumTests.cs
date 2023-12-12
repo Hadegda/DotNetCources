aba53d3ab11ab5844e471143ca08bea88ad4b036
nadezhda <katsman96@yandex.ru>
2023-12-12 21:30:51 +0400

    Add unit test for negative and fractional numbers

--- M7_UnitTesting/UnitTesting/StringSumTests/StringSumTests.cs
+++ M7_UnitTesting/UnitTesting/StringSumTests/StringSumTests.cs
@@ -21,8 +21,8 @@ namespace UnitTesting.StringSum.Tests
 
 		[TestMethod]
 		[DataRow("3", "8", "11")]
-		[DataRow("0", "1", "1")]
-		[DataRow("1", "0", "1")]
+		[DataRow("0", "+1", "1")]
+		[DataRow("1", "+0", "1")]
 		public void SumTest_ValidArguments_CountCorrectly(string num1, string num2, string expectedValue)
 		{
 			var actualValue = StringSum.Sum(num1, num2);
@@ -36,6 +36,19 @@ namespace UnitTesting.StringSum.Tests
 			var expectedValue = ((long)int.MaxValue + int.MaxValue).ToString();
 
 			var actualValue = StringSum.Sum(int.MaxValue.ToString(), int.MaxValue.ToString());
+
+			Assert.AreEqual(expectedValue, actualValue);
+		}
+
+		[TestMethod]
+		[DataRow("3", "-8", "3")]
+		[DataRow("-8", "3", "3")]
+		[DataRow("3", "2.5", "3")]
+		[DataRow("2.5", "3", "3")]
+		[DataRow("-2.5", "-2.5", "0")]
+		public void SumTest_SumWithNotNaturalValue_CountCorrectly(string num1, string num2, string expectedValue)
+		{
+			var actualValue = StringSum.Sum(num1, num2);
 
 			Assert.AreEqual(expectedValue, actualValue);
 		}
