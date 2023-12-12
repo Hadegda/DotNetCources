6076cefa1cdf69c44fc0b3957552055a05aa6c16
nadezhda <katsman96@yandex.ru>
2023-12-12 20:38:52 +0400

    Improve string Sum(string num1, string num2) to pass existed unit tests

--- M7_UnitTesting/UnitTesting/UnitTesting/StringSum.cs
+++ M7_UnitTesting/UnitTesting/UnitTesting/StringSum.cs
@@ -1,9 +1,17 @@
-﻿namespace UnitTesting.StringSum
+﻿using static System.Net.Mime.MediaTypeNames;
+using System;
+
+namespace UnitTesting.StringSum
 {
 	public static class StringSum
 	{
 		public static string Sum(string num1, string num2)
 		{
+			if (int.TryParse(num1, out int number1) || int.TryParse(num2, out int number2))
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+
 			return "";
 		}
 	}
